bd2f1ede5dc14bdcc288c845cf6ae5ea59fd6c38
A-Mallik <42754363+A-Mallik@users.noreply.github.com>
2018-11-16 19:29:23 -0500

    Update shohan.js
    Added reddit

--- assets/js/shohan.js
+++ assets/js/shohan.js
@@ -18,8 +18,87 @@ window.onload = function() {
                  $("#wikiInfo").html(info); //where the summary is shown on the page
         });
 
-      });
 
 
+
+
+
+      // =====================Reddit API==========================================
+
+      var queryURL = "https://api.reddit.com/search?q=" + $("#search-query").val().trim(); // set limit to 10
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function(response) {
+          $('.reddit_cards').empty();
+            var results = response.data;
+            console.log(results);                    //put response in a variable
+            if(($("#search-query").val().trim()) != ""){
+              $('.reddit_cards').empty();
+            for(var i =0; i < 25; i++){
+              var title=results.children[i].data.title;
+              var url=results.children[i].data.url;
+
+              var image=results.children[i].data.thumbnail;
+              if(image === "default" || image === "self"){
+              image = "assets/images/reddit-logo.svg"
+            }
+
+              var redditCard = $('<tr id=' + url + '><td><h6><a href="' + url + '"target="_blank">' + title + '</a></h6><p>' + " " + '</p></td><td class="d-flex justify-content-end"><img src=' + image +' style="height:90px; width:90px;"></td></tr>');
+              $('.reddit_cards').append(redditCard);
+
+
+            }
+
+          } // if statement
+
+          else{
+            console.log("yes");
+          }
+
+              console.log(results.children[14].data.url);
+        });
+
+
+        // =====================Reddit API==========================================
+        });
+
+        var queryURL = "https://api.reddit.com/r/worldnews"; // set limit to 10
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(response) {
+            $('.reddit_cards').empty();
+              var results = response.data;
+              console.log(results);                    //put response in a variable
+              // if(($("#search-query").val().trim()) != ""){
+                $('.reddit_cards').empty();
+              for(var i =0; i < 25; i++){
+                var title=results.children[i].data.title;
+                var url=results.children[i].data.url;
+
+                var image=results.children[i].data.thumbnail;
+                if(image === "" || image === "self"){
+                image = "assets/images/reddit-logo.svg"
+              }
+
+                var redditCard = $('<tr id=' + url + '><td><h6><a href="' + url + '"target="_blank">' + title + '</a></h6><p>' + " " + '</p></td><td class="d-flex justify-content-end"><img src=' + image +' style="height:90px; width:90px;"></td></tr>');
+                $('.reddit_cards').append(redditCard);
+
+
+              }
+
+            // } // if statement
+
+            // else{
+            //   console.log("yes");
+            // }
+
+                console.log(results.children[14].data.url);
+          });
 
 }
